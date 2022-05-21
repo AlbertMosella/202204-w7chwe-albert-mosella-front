efff1f592adf4fe5304c7fbb4be80608e37d8083
AlbertMosella <mosella.albert@gmail.com>
2022-05-21 19:04:52 +0200

    Added test to user and users list component

---
+++ src/components/UsersList/UsersList.test.js
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { mockUser, mockUsers } from "../../mocks/users";
+import store from "../../redux/store/store";
+import UsersList from "./UsersList";
+
+describe("Given a UsersList Component", () => {
+  describe("When it's called with a User", () => {
+    test("Then it should render a li element'", () => {
+      const expectedResult = 1;
+
+      render(
+        <Provider store={store}>
+          <UsersList users={[mockUser]} />
+        </Provider>
+      );
+
+      const receivedResult = screen.getAllByRole("list");
+      expect(receivedResult).toHaveLength(expectedResult);
+    });
+  });
+});
